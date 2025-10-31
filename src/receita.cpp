7afdd4c040e93b555c24c8a6d7d6e1232c61feed
diogo-tc <diogotc2207@gmail.com>
2025-10-30 23:08:39 -0300

    Delete src directory

--- src/receita.cpp
+++
@@ -1,9 +0,0 @@
-#include "receita.h"
-
-Receita::Receita(Paciente p, std::string medicoNome, std::string medicamento)
-    : paciente(p), medicoNome(medicoNome), medicamento(medicamento) {}
-
-std::string Receita::getDescricao() {
-    return "Receita para " + paciente.getNome() + " emitida por " + medicoNome +
-           ": " + medicamento;
-}
